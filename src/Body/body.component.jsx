6e3eef754dd07f233e8f51c6015d302c0cc49dc1
Oluwatosin-Bakare <oluwatosin.bakare@yahoo.com>
2021-10-11 13:26:02 +0100

    Arrow

--- src/Body/body.component.jsx
+++ src/Body/body.component.jsx
@@ -3,13 +3,18 @@ import BoxOne from '../BoxOne/BoxOne.component';
 import BoxTwo from '../BoxTwo/BoxTwo.component';
 import BoxThree from '../BoxThree/BoxThree.component';
 import BoxFour from '../BoxFour/BoxFour.component';
+import {BiArrowBack } from 'react-icons/bi'
 import './body.styles.css';
 
 const Body = () => (
     <div className='body'>
-        
-        <h1 className='New'>New Booking</h1>
-        <p className='fill'>Fill in the information to get a quote or create a new booking</p>
+        <div className='ArrowWords'>
+             <BiArrowBack className='Back-Arrow' />
+        <div className='words'>
+             <h1 className='New'>New Booking</h1>
+              <p className='fill'>Fill in the information to get a quote or create a new booking</p>
+        </div>
+        </div>
         <BoxOne />
         <BoxTwo />
         <BoxThree />
