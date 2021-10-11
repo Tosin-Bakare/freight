c7451a4d1c45c1f869103143eaba9031b4258de8
Oluwatosin-Bakare <oluwatosin.bakare@yahoo.com>
2021-10-11 13:35:12 +0100

    Freight

--- src/Body/body.component.jsx
+++ src/Body/body.component.jsx
@@ -3,18 +3,12 @@ import BoxOne from '../BoxOne/BoxOne.component';
 import BoxTwo from '../BoxTwo/BoxTwo.component';
 import BoxThree from '../BoxThree/BoxThree.component';
 import BoxFour from '../BoxFour/BoxFour.component';
-import {BiArrowBack } from 'react-icons/bi'
 import './body.styles.css';
 
 const Body = () => (
     <div className='body'>
-        <div className='ArrowWords'>
-             <BiArrowBack className='Back-Arrow' />
-        <div className='words'>
              <h1 className='New'>New Booking</h1>
               <p className='fill'>Fill in the information to get a quote or create a new booking</p>
-        </div>
-        </div>
         <BoxOne />
         <BoxTwo />
         <BoxThree />
